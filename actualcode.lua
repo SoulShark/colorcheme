45981843077827fc952e180fa01e966e76483b47
Bob Blackmon <bob.blackmon@ymail.com>
2017-04-19 18:07:10 -0400

    Revision
    Made things prettier.
    Removed the compound convar and just used the r,g,b ones since the compound one isn't necessary.
    Changed RunConsoleCommand to use the numbers, not the unnecessary strings.
    Frame now closes when you push Save Color.
    
    I recommend looking at the source code for DColorMixer:SetConVar()

--- actualcode.lua
+++ actualcode.lua
@@ -15,51 +15,35 @@ if SERVER then
     hook.Add("PlayerSay", "ColorMixerOpen", function(ply, newtext)
         local newtext = string.lower(newtext)
 
-            // This is where you can add ranks to access th color changer.
-            // PLEASE keep to the format provided below.
+        // This is where you can add ranks to access th color changer.
+        // PLEASE keep to the format provided below.
 
 
-if ply:IsUserGroup("founder")
-or ply:IsUserGroup("admin")
-or ply:IsUserGroup("founder")
-or ply:IsUserGroup("superadmin")
-or ply:IsUserGroup("user")
-
-and (string.sub(newtext, 1, 7) == "!hvcolor")
-
-
-         then
+        if ply:IsUserGroup("founder")
+        or ply:IsUserGroup("admin")
+        or ply:IsUserGroup("founder")
+        or ply:IsUserGroup("superadmin")
+        or ply:IsUserGroup("user")
+        and (string.sub(newtext, 1, 7) == "!hvcolor") then
             net.Start("OpenMixer")
             net.Send(ply)
             return ""
-            else return "You are not the right user group to use this command."
-end
+        else 
+            return "You are not the right user group to use this command."
+        end
     end)
 end
 
 
 if CLIENT then
-    local hv_colorbase = CreateClientConVar("hex_color", "255, 255, 0", true, true)
-    local hv_colorred = CreateClientConVar("hex_colorred", "255", true, true)
-    local hv_colorgreen = CreateClientConVar("hex_colorgreen", "255", true, true)
-    local hv_colorblue = CreateClientConVar("hex_colorblue", "255", true, true)
+    --local hv_colorbase = CreateClientConVar("hex_color", "255, 255, 0", true, true)
+    local hv_colorred = CreateClientConVar("hex_colorred", "255", true, false)
+    local hv_colorgreen = CreateClientConVar("hex_colorgreen", "255", true, false)
+    local hv_colorblue = CreateClientConVar("hex_colorblue", "255", true, false)
     local alpha = 0
 
     local function GrabColor()
-        local coltable = string.Explode(",", hv_colorbase:GetString())
-        local newcol = {}
-
-        for k, v in pairs(coltable) do
-            v = tonumber(v)
-
-            if v == nil then
-                coltable[k] = 0
-            end
-        end
-
-        newcol[1], newcol[2], newcol[3] = coltable[1] or 0, coltable[2] or 0, coltable[3] or 0
-
-        return Color(newcol[1], newcol[2], newcol[3])
+        return Color(hv_colorred:GetInt(), hv_colorgreen:GetInt(), hv_colorblue:GetInt())
     end
 
     net.Receive("OpenMixer", function(len, ply)
@@ -81,13 +65,11 @@ if CLIENT then
 
         Butt.DoClick = function(Butt)
             local colors = colMix:GetColor()
-            local colstringred = tostring(colors.r)
-            local colstringgreen = tostring(colors.g)
-            local colstringblue = tostring(colors.b)
-            RunConsoleCommand("hex_color", colstringred .. "," .. colstringgreen .. "," .. colstringblue)
-            RunConsoleCommand("hex_colorred", colstringred)
-            RunConsoleCommand("hex_colorgreen", colstringgreen)
-            RunConsoleCommand("hex_colorblue", colstringblue)
+            --RunConsoleCommand("hex_color", colstringred .. "," .. colstringgreen .. "," .. colstringblue)
+            RunConsoleCommand("hex_colorred", colors.r)
+            RunConsoleCommand("hex_colorgreen", colors.g)
+            RunConsoleCommand("hex_colorblue", colors.b)
+            Frame:Close()
         end
     end)
 else
